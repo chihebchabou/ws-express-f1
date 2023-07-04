b5c2546f45cdff377350d16e814450dc2867c316
chihebchabou <chihebchaboutech@gmail.com>
2023-07-04 19:02:25 +0100

    create server using express

--- server.js
+++ server.js
@@ -1,69 +1,78 @@
 // Dependencies
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
+// Initialize application
+const app = express();
+
+// User Data
 const users = [
   { id: 1, fullName: 'John Doe', email: 'john@gmail.com' },
   { id: 2, fullName: 'Jane Doe', email: 'jane@gmail.com' },
   { id: 3, fullName: 'Sam Smith', email: 'sam@gmail.com' },
 ];
 
-// Create HTTP server
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (!err) {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content);
-        } else {
-          console.log(err);
-          res.writeHead(500, { 'Content-Type': 'text/html' });
-          res.end('<h2>500 Server Error</h2>');
-        }
-      }
-    );
-  } else if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-        if (!err) {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content);
-        } else {
-          res.writeHead(500, { 'Content-Type': 'text/html' });
-          res.end('<h2>500 Server Error</h2>');
-        }
-      }
-    );
-  } else if (req.url === '/contact') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'contact.html'),
-      (err, content) => {
-        if (!err) {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content);
-        } else {
-          res.writeHead(500, { 'Content-Type': 'text/html' });
-          res.end('<h2>500 Server Error</h2>');
-        }
-      }
-    );
-  } else if (req.url === '/api/users') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(users));
+// app.get('/', (req, res) => {
+//   console.log(req.hostname);
+//   console.log(req.method);
+//   console.log(req.get('Content-Type'));
+//   // res.send('<h1>Home Page</h1>');
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'), err => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send('500 Server error');
+//     } else {
+//       console.log('Success');
+//     }
+//   });
+// });
+
+// app.get('/about', (req, res) => {
+//   // res.send('<h1>About Page</h1>');
+//   res.sendFile(path.join(__dirname, 'public', 'about.html'), err => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send('500 Server error');
+//     } else {
+//       console.log('Success');
+//     }
+//   });
+// });
+
+// app.get('/contact', (req, res) => {
+//   // res.send('<h1>Contact Page</h1>');
+//   res.sendFile(path.join(__dirname, 'public', 'contact.html'), err => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send('500 Server error');
+//     } else {
+//       console.log('Success');
+//     }
+//   });
+// });
+
+// Define authorize middleware
+const authorize = (req, res, next) => {
+  const isAuth = true;
+  if (isAuth) {
+    req.users = users;
+    next();
   } else {
-    res.writeHead(404);
-    res.end('<h2>404 Not Found</h2>');
+    res.status(401).send('401 Not Authorized');
   }
+};
+
+// Middleware use
+app.use(authorize);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/users', (req, res) => {
+  console.log(req);
+  res.send(req.users);
 });
 
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
-
+// Define port
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+// Listen to the port
+app.listen(port, () => console.log(`Server is running on port ${port}`));
